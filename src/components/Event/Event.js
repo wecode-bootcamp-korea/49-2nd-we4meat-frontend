f273eb1783b28693a396c759349c9fef2d1e8969
philamones <philamones@icloud.com>
2023-09-21 00:08:43 +0900

    [style] 이벤트 swiper 컴포넌트 개발

--- src/components/Event/Event.js
+++ src/components/Event/Event.js
@@ -1,39 +1,67 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay, Navigation, Pagination } from 'swiper/modules';
+import { Navigation, Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import './Event.scss';
 
-function Event() {
-  const pagination = {
-    clickable: true,
-    renderBullet: function (index, className) {
-      return `<span className="`;
-
-      // '<span class="' + className + '">' + (index + 1) + '</span>';
-    },
-  };
+const Event = () => {
+  const swiperRef = useRef();
 
   return (
-    <Swiper
-      modules={[Autoplay, Navigation, Pagination]}
-      autoplay={{ delay: 2000, disableOnInteraction: false }}
-      navigation
-      pagination={pagination}
-      loop={true}
-    >
-      <SwiperSlide>Event 1</SwiperSlide>
-      <SwiperSlide>Event 2</SwiperSlide>
-      <SwiperSlide>Event 3</SwiperSlide>
-      <SwiperSlide>Event 4</SwiperSlide>
-      <SwiperSlide>Event 5</SwiperSlide>
-      <SwiperSlide>Event 6</SwiperSlide>
-      <SwiperSlide>Event 7</SwiperSlide>
-      <SwiperSlide>Event 8</SwiperSlide>
-    </Swiper>
+    <div className="event">
+      <Swiper
+        modules={[Navigation, Pagination]}
+        loop
+        navigation={{
+          prevEl: '.swiper-prev-btn',
+          nextEl: '.swiper-next-btn',
+        }}
+        pagination={{
+          el: '.event-swiper-pagination',
+          type: 'custom',
+          renderCustom: function (swiper, current, total) {
+            return `<span>${current}</span> / ${total}`;
+          },
+        }}
+      >
+        <SwiperSlide>
+          <img src="/images/swiper/meat1.jpg" alt="1번 고기 이미지" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="/images/swiper/meat2.jpg" alt="2번 고기 이미지" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="/images/swiper/meat3.jpg" alt="3번 고기 이미지" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="/images/swiper/meat4.jpg" alt="4번 고기 이미지" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="/images/swiper/meat5.jpg" alt="5번 고기 이미지" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="/images/swiper/meat6.jpg" alt="6번 고기 이미지" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="/images/swiper/meat7.jpg" alt="7번 고기 이미지" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="/images/swiper/meat8.jpg" alt="8번 고기 이미지" />
+        </SwiperSlide>
+        <div className="controller">
+          <button type="button" className="swiper-prev-btn">
+            Prev
+          </button>
+          <span className="event-swiper-pagination"></span>
+          <button type="button" className="swiper-next-btn">
+            Next
+          </button>
+        </div>
+      </Swiper>
+    </div>
   );
-}
+};
 
 export default Event;
