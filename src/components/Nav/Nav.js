d3606571401938832c8e311eac5a0057926a992d
heejii <heejii97@naver.com>
2023-09-20 16:36:38 +0900

    fix: label태그 추가 및 props수정

--- src/components/Nav/Nav.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './Nav.scss';
-
-const Nav = () => {
-  return <>navigation</>;
-};
-
-export default Nav;
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import './Main.scss';
 
+import Input from '../../components/Input/Input';
+
 const Main = () => {
-  return <div className="main"></div>;
+  return (
+    <div className="main">
+      <Input />
+    </div>
+  );
 };
 
 export default Main;
