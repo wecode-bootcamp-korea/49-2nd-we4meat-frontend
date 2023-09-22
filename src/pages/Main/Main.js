4384fc876379f3ecef31299830ad900ca07f5d1c
이영은 <hisenselee12@gmail.com>
2023-09-22 20:56:32 +0900

    [fix]review 수정2

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -3,14 +3,12 @@ import BigBanner from './BigBanner/BigBanner';
 import EventBanners from './EventBanners/EventBanners';
 import ListBanner from './ListBanner/ListBanner';
 import './Main.scss';
-import OptionElement from '../../components/OptionElement/OptionElement';
 
 const Main = () => {
   return (
     <main className="main">
       <BigBanner />
       <EventBanners />
-      <OptionElement category="구이용" />
       {/* 제품 목록 컴포넌트 공간 */}
       <ListBanner />
     </main>
