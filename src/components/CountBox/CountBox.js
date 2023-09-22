845d8f330800e27101d678cc2d11a2f82157814d
이영은 <hisenselee12@gmail.com>
2023-09-22 17:02:01 +0900

    [style]수량선택 component 완료

--- src/components/CountBox/CountBox.js
+++ src/components/CountBox/CountBox.js
@@ -1,28 +1,42 @@
-import React, { useState } from 'react';
-import CountSelect from '../CountSelect/CountSelect';
+import React from 'react';
 import './CountBox.scss';
 
-const CountBox = () => {
-  const [count, setCount] = useState(1);
-  const handleMinusCount = () => {
-    if (count === 1) {
-      alert('최소 수량입니다.');
-    } else {
-      setCount(count - 1);
-    }
-  };
-  const handlePlusCount = () => {
-    setCount(count + 1);
-  };
+const CountBox = props => {
+  const { count, plus, minus, scale, text } = props;
+  // 사용하고자 하는 곳에 선언
+  // const [count, setCount] = useState(1);
+  // const handleMinusCount = () => {
+  //   if (count === 1) {
+  //     alert('최소 수량입니다.');
+  //   } else {
+  //     setCount(count - 1);
+  //   }
+  // };
+  // const handlePlusCount = () => {
+  //   setCount(count + 1);
+  // };
+
+  // Props
+  // count: [state]
+  // plus: [function] handlePlusCount
+  // minus: [function] handleMinusCount
+  // scale: [string] full, small
+  // text: [string] 입력하고자하는 값, 없으면 ''
 
   return (
-    <div className="count-wrap">
-      <p>수량</p>
-      <CountSelect
-        count={count}
-        plus={handlePlusCount}
-        minus={handleMinusCount}
-      />
+    <div className="count-wrap" scale={scale}>
+      {text.length !== 0 && <p>{text}</p>}
+      <div className="count-select-wrap" scale={scale}>
+        <button scale={scale} onClick={minus}>
+          -
+        </button>
+        <p className="op-name" scale={scale}>
+          {count}
+        </p>
+        <button scale={scale} onClick={plus}>
+          +
+        </button>
+      </div>
     </div>
   );
 };
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -3,13 +3,11 @@ import BigBanner from '../../components/BigBanner/BigBanner';
 import EventBanners from '../../components/EventBanners/EventBanners';
 import ListBanner from '../../components/ListBanner/ListBanner';
 import './Main.scss';
-import CountSelect from '../../components/CountSelect/CountSelect';
 
 const Main = () => {
   return (
     <main className="main">
       <BigBanner />
-      <CountSelect scale="full" />
       <EventBanners />
       {/* 제품 목록 컴포넌트 공간 */}
       <ListBanner />
