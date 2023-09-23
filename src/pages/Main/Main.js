55ec7598c4f3d73f113ee0df7555aece5be0c4df
heejii <heejii97@naver.com>
2023-09-23 14:14:14 +0900

    products 컴포넌트 개발중

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import BigBanner from './BigBanner/BigBanner';
 import EventBanners from './EventBanners/EventBanners';
+import Products from '../../components/Products/Products';
 import ListBanner from './ListBanner/ListBanner';
 import './Main.scss';
 
@@ -9,6 +10,7 @@ const Main = () => {
     <main className="main">
       <BigBanner />
       <EventBanners />
+      <Products />
       {/* 제품 목록 컴포넌트 공간 */}
       <ListBanner />
     </main>
