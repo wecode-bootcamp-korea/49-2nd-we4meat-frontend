dc169f7fcb077e6c27131122c8be3042ff42ac8f
ryushin0 <141554987+ryushin01@users.noreply.github.com>
2023-09-25 20:21:28 +0900

    [feat] 등급제 회원 mock data 생성

--- src/pages/Mypage/UserInfoOverview/UserInfoOverview.js
+++ src/pages/Mypage/UserInfoOverview/UserInfoOverview.js
@@ -2,6 +2,28 @@ import React from 'react';
 import './UserInfoOverview.scss';
 
 const UserInfoOverview = () => {
+  const USER_RANK = [
+    {
+      rank: 'VIP',
+      path: "process.env.PUBLIC_URL + '/images/mypage/rank_vip.png",
+      text: 'VIP 등급 회원',
+    },
+    {
+      rank: '골드',
+      path: "process.env.PUBLIC_URL + '/images/mypage/rank_gold.png",
+      text: '골드 등급 회원',
+    },
+    {
+      rank: '실버',
+      path: "process.env.PUBLIC_URL + '/images/mypage/rank_silver.png",
+      text: '실버 등급 회원',
+    },
+    {
+      rank: '웰컴',
+      path: "process.env.PUBLIC_URL + '/images/mypage/rank_welcome.png",
+      text: '웰컴 등급 회원',
+    },
+  ];
   return (
     <section className="user-info-overview">
       <div>
