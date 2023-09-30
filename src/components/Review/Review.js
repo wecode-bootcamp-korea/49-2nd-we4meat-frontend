87bec7162721e9c84f3d24199292c77c7fa457cd
ryushin0 <141554987+ryushin01@users.noreply.github.com>
2023-09-30 15:23:46 +0900

    [style] 리뷰 목록 더보기 버튼 추가

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -31,41 +31,49 @@ const Review = () => {
   // 더보기 버튼: 4개씩 추가 노출(기본 8개)
 
   return (
-    <div className="review-list">
-      {dataList?.map((item, index) => {
-        return (
-          <div className="review-item" key={index}>
-            <div className="review-wrap">
-              <div className="left-area">
-                <strong>{item.title}</strong>
-                <textarea value={item.content} readOnly />
-                <div className="metadata">
-                  <span>
-                    <span>{item.user_name}</span>
-                    <em>{item.user_buy}회 구매</em>
-                  </span>
-                  <span>{item.date}</span>
+    <>
+      <div className="review-list">
+        {dataList?.map((item, index) => {
+          return (
+            <div className="review-item" key={index}>
+              <div className="review-wrap">
+                <div className="left-area">
+                  <strong>{item.title}</strong>
+                  <textarea value={item.content} readOnly />
+                  <div className="metadata">
+                    <span>
+                      <span>{item.user_name}</span>
+                      <em>{item.user_buy}회 구매</em>
+                    </span>
+                    <span>{item.date}</span>
+                  </div>
                 </div>
-              </div>
 
-              {/* 이미지 있는 경우만 조건부 렌더링 필요 */}
-              {item.HasImage && (
-                <div className="right-area">
-                  <img
-                    src={item.image_url}
-                    alt={`${item.user_name} 님의 리뷰 사진`}
-                  />
-                </div>
-              )}
+                {/* 이미지 있는 경우만 조건부 렌더링 필요 */}
+                {item.HasImage && (
+                  <div className="right-area">
+                    <img
+                      src={item.image_url}
+                      alt={`${item.user_name} 님의 리뷰 사진`}
+                    />
+                  </div>
+                )}
+              </div>
+              <div className="additional-info">
+                <span>
+                  함께 구매하신 상품 &#40;{item?.together.length}&#41;
+                </span>
+                <ul>{makeBuyTogether(item?.together)}</ul>
+              </div>
             </div>
-            <div className="additional-info">
-              <span>함께 구매하신 상품 &#40;{item?.together.length}&#41;</span>
-              <ul>{makeBuyTogether(item?.together)}</ul>
-            </div>
-          </div>
-        );
-      })}
-    </div>
+          );
+        })}
+      </div>
+
+      <button type="button" className="btn-review-more">
+        <span>더보기</span>
+      </button>
+    </>
   );
 };
 
