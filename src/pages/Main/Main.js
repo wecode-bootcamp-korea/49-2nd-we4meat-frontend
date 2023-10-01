8e05cee592c62f09d7cc6356d3b6eed86dafdf4b
이영은 <hisenselee12@gmail.com>
2023-10-02 06:40:05 +0900

    [flx]main.js 확인용 코드 삭제

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -3,14 +3,12 @@ import BigBanner from './BigBanner/BigBanner';
 import EventBanners from './EventBanners/EventBanners';
 import Products from '../../components/Products/Products';
 import ListBanner from './ListBanner/ListBanner';
-import CategoryTab from '../../components/CategoryTab/CategoryTab';
 
 const Main = () => {
   return (
     <main id="main" className="main">
       <BigBanner />
       <EventBanners />
-      <CategoryTab />
       <Products />
       {/* 제품 목록 컴포넌트 공간 */}
       <ListBanner />
