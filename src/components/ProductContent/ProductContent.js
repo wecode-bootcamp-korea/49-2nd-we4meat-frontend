86f457a8476e0e563d20a9b8326bf5280d5d0d3c
ryushin0 <141554987+ryushin01@users.noreply.github.com>
2023-10-02 22:30:14 +0900

    [fix] 모달 팝업 수정

--- src/components/ProductContent/ProductContent.js
+++ src/components/ProductContent/ProductContent.js
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Tag from '../Tag/Tag';
 import ProductImg from '../ProductListImg/ProductImg';
 import CartButton from '../CartButton/CartButton';
+import Modal from '../Modal/Modal';
 import './ProductContent.scss';
 
 function ProductContent({ className, img, inventory, title }) {
+  const [modalOpen, setModalOpen] = useState(false);
+
   return (
     <div
       className={`product-content ${className} ${
         inventory === 2 ? 'inventory' : ''
       }`}
     >
-      {title.includes('무항g생제') ? <Tag /> : ''}
+      {title.includes('무항생제') ? <Tag /> : ''}
       <ProductImg img={img} />
-      <CartButton />
+
+      <CartButton onClick={() => setModalOpen(true)} />
+      {modalOpen && (
+        <>
+          <div className="modal-backdrop" onClick={() => setModalOpen(false)} />
+          <section className="modal-popup" data-size="xs">
+            <button type="button" onClick={() => setModalOpen(false)}>
+              닫기
+            </button>
+            <Modal index="0" />
+          </section>
+        </>
+      )}
     </div>
   );
 }
