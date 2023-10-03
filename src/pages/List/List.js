270c915e5a4676680f16dd8dcf87c60b5c3eed42
ryushin0 <141554987+ryushin01@users.noreply.github.com>
2023-10-03 14:12:10 +0900

    [style] [feat] 제품 목록 페이지 개발

---
+++ src/pages/List/List.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useSearchParams } from 'react-router-dom';
+import CategoryBanner from './CategoryBanner/CategoryBanner';
+import CategoryTab from '../../components/CategoryTab/CategoryTab';
+import Products from '../../components/Products/Products';
+import CATEGORY_NAME from '../../data/categoryImgData';
+import './List.scss';
+
+const List = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const category = searchParams.get('category');
+  const index = CATEGORY_NAME.findIndex(i => i.englishText === category);
+  const title = CATEGORY_NAME[index].bannerText;
+  const image = CATEGORY_NAME[index].bannerImg;
+
+  return (
+    <main id="main" className="list">
+      <CategoryBanner title={title} image={image} />
+      <section className="category-tab">
+        <div className="inner-wrap">
+          <CategoryTab />
+        </div>
+      </section>
+      <Products />
+    </main>
+  );
+};
+
+export default List;
