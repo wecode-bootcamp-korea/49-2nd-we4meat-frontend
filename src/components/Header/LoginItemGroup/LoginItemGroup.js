bf28cd36d1bdb6f6ad67ac5dd1ad4cd0aa5ff8b9
이영은 <hisenselee12@gmail.com>
2023-10-05 22:33:22 +0900

    [style]header/login 상태 ui 변환

--- src/components/Header/LoginItemGroup/LoginItemGroup.js
+++ src/components/Header/LoginItemGroup/LoginItemGroup.js
@@ -9,13 +9,18 @@ const LINK_LIST = [
   { id: 2, text: '회원가입', path: '/sign-up' },
 ];
 
+const isLogin = localStorage.getItem('accessToken');
+
 const LoginItemGroup = () => {
   return (
     <ul className="login-list">
-      {LINK_LIST.map(link => {
-        return <Nav key={link.id} text={link.text} path={link.path} />;
-      })}
-      {/* <Nav text="마이페이지" path="mypage"/> 토큰 있을때 변환 */}
+      {!isLogin ? (
+        LINK_LIST.map(link => {
+          return <Nav key={link.id} text={link.text} path={link.path} />;
+        })
+      ) : (
+        <Nav text="마이페이지" path="mypage" />
+      )}
     </ul>
   );
 };
